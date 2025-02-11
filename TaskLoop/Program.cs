5ae53266f5821752181305bc68e5d040c9e89a0c
gennadii_brukhno <gennadii.brukhno@altenar.com>
2025-02-11 09:18:20 +0300

    Add project files.

---
+++ TaskLoop/Program.cs
@@ -0,0 +1,45 @@
+﻿var taskLoop = new TaskLoop
+{
+    A = () => Console.WriteLine($"After delay {Environment.CurrentManagedThreadId}"),
+    Max = 5,
+};
+
+
+Console.WriteLine($"Hello world {Environment.CurrentManagedThreadId}");
+taskLoop.Run();
+taskLoop.Task.Wait();
+
+class TaskLoop
+{
+    public required Action A { get; init; }
+    public required Action B { get; init; }
+    public required int Max { get; init; }
+
+    public Task Task { get; private set; } = null!;
+
+    public void Run()
+    {
+        if (Task is not null)
+        {
+
+            throw new InvalidOperationException("Task has been alredy run");
+        }
+
+        var tcs = new TaskCompletionSource();
+
+        Task = tcs.Task;
+
+        var task0 = new Task(A);
+
+        var task1 = task0;
+
+        for (var i = 1; i < Max; i++)
+        {
+            task1 = task1.ContinueWith(_ => A());
+        }
+
+        task1.ContinueWith(_ => tcs.SetResult());
+
+        task0.Start();
+    }
+}
---
+++ TimerDelay/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Diagnostics;
+
+var sw = Stopwatch.StartNew();
+
+var task = MyTimer.Delay(1500);
+
+Console.WriteLine($"{sw.ElapsedMilliseconds}: Delay started");
+
+task.Wait();
+
+Console.WriteLine($"{sw.ElapsedMilliseconds}: Delay finished");
+
+public class MyTimer
+{
+    public static Task Delay(int milliseconds)
+    {
+        var tcs = new TaskCompletionSource();
+
+        var timer = new MyTimer(milliseconds, tcs);
+
+        return tcs.Task;
+    }
+
+    private MyTimer(int milliseconds, TaskCompletionSource tcs)
+    {
+        _milliseconds = milliseconds;
+        _tcs = tcs;
+
+        _timer = new Timer(
+            Callback,
+            null,
+            _milliseconds,
+            Timeout.Infinite);
+    }
+
+    private void Callback(object? _)
+    {
+        _timer.Dispose();
+        _tcs.SetResult();
+    }
+
+    private int _milliseconds;
+    private readonly TaskCompletionSource _tcs;
+    private readonly Timer _timer;
+}
